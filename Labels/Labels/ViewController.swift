d2dc7853002ca20f434d3417245d15309f161b76
vloss <viniciusvianaloss@gmail.com>
2023-01-16 15:09:08 -0300

    Revisão de textos com UILabel

---
+++ Labels/Labels/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Labels
+//
+//  Created by Vinicius Loss on 16/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
