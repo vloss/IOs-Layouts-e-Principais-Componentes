fb1ef93a8d9bd0fcaa271bd871abb4d655c67234
vloss <viniciusvianaloss@gmail.com>
2023-01-19 10:05:31 -0300

    Iniciando App de Mensagens. Para revisão de componentes e delegate.

---
+++ Mensagens/Mensagens/BaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BaseViewController.swift
+//  Mensagens
+//
+//  Created by Vinicius Loss on 19/01/23.
+//  Copyright © 2023 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
