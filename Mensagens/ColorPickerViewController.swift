8c0a6858b8fc26a91c67b4568fa9ac2bda3df435
vloss <viniciusvianaloss@gmail.com>
2023-01-19 22:35:56 -0300

    Criação de tela ColorPicker com View Controller e ligação dos Outlets.

---
+++ Mensagens/ColorPickerViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ColorPickerViewController.swift
+//  Mensagens
+//
+//  Created by Vinicius Loss on 19/01/23.
+//  Copyright © 2023 Eric Brito. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerViewController: UIViewController {
+
+    @IBOutlet weak var viColor: UIView!
+    
+    @IBOutlet weak var slRed: UISlider!
+    @IBOutlet weak var slGreen: UISlider!
+    @IBOutlet weak var slBlue: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
