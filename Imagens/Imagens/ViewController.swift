36ecfd1142b87a7f4f336262e14ed11a7e04733e
vloss <viniciusvianaloss@gmail.com>
2023-01-21 16:06:14 -0300

    Iniciando revisão de componente ImageView.

---
+++ Imagens/Imagens/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Imagens
+//
+//  Created by Vinicius Loss on 21/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
